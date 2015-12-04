f0d2c4150c3bc96b4bd0f717bc6ed5ac9d3f8291
Christian Stade-Schuldt <christian.stade-schuldt@project-a.com>
2015-12-04 16:32:19 +0100

    add prices

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ def get_mensa_food():
     f = fp.parse('http://www.studentenwerk-berlin.de/speiseplan/rss/hu_nord/tag/kurz/0')
     tree = html.fromstring(f['entries'][0]['summary'])
     food_items = tree.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "mensa_day_speise_name", " " ))]')
-    food_list = [{'color': colors[x.xpath('./a/@href')[0].split('_')[1]],
-                  'text': (re.sub('\d{2,}\w*\d*', '', x.text_content().strip())).rstrip()} for x in food_items]
+    food_prices = tree.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "mensa_day_speise_preis", " " ))]')
+    food_list = [{'color': colors[x[0].xpath('./a/@href')[0].split('_')[1]],
+                  'text': (re.sub('\d{2,}\w*\d*', '', x[0].text_content().strip())).rstrip() + ' '
+                          + x[1].text_content().rstrip()} for x in list(zip(food_items, food_prices))]
 
     payload = {"text": 'Today at Mensa',
                'attachments': food_list}
